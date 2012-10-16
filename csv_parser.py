5408a88ec4ac2ea3faf1dd27085f62b180bd63d0
javier <javierhonduco@gmail.com>
2012-10-17 00:12:41 +0200

    1st commit

---
+++ csv_parser.py
@@ -0,0 +1,30 @@
+# There should be thousands of csv parsers out there, but I'm lazy you know ;)
+# @author: @javierhonduco
+import sys
+
+class CsvParser:
+	
+	def __init__(self, file, becool=0):
+		self.becool = becool
+		self.dataStream = open(file, "r").read()
+		self.date_format = "%d/%M/%Y, %H:%S"
+
+	def proprocess(self):
+
+		return [(int(line.split(",")[0]), self.prettify(int(line.split(",")[1]))) for line in self.dataStream.split("\n")] # creates the list...
+
+	def jsonize(self):
+		import json
+		return json.dumps(self.proprocess()) # converts into a json string
+
+	def prettify(self, WAT):
+		from datetime import datetime
+		if self.becool == 1:
+			return datetime.fromtimestamp(WAT).strftime(self.date_format) # You hate *NIX timestamps madafaka? ;)
+		else:
+			return WAT
+if __name__ == "__main__":
+		parser = CsvParser(sys.argv[1].split("::")[0], int(sys.argv[1].split("::")[1]))  # LOL
+		print parser.jsonize()
+
+
---
+++ redbull.py
@@ -0,0 +1,34 @@
+# This is an extremely dirty, non OOP, done in 6 minutes script to fetch live redbull stratos viewers 
+# @author: @javierhonduco
+
+#from bs4 import BeautifulSoup # I firstly thought data wasn't loaded vua js :b
+import datetime, time, urllib2, tweepy
+
+ENABLE_TWEETING = False # Pure crap way... Some uneeded stuff is being loaded but... rough prototyping! :)
+REQUEST_INTERVALE = 600
+OUTPUT = os.basename(__file__) + "/whatever.csv"
+
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+oldata = 0
+api = tweepy.API(auth)
+
+while 1: 
+	data = int(urllib2.urlopen("http://www.youtube.com/live_stats?v=MrIxH6DToXQ&t=1350225339022").read()) # A non documented API endopoint... Assuming that no errors will be raised :P
+	#soup = BeautifulSoup(data)
+	#content = soup.find_all("span", class_= "concurrent-viewers-number")
+	time = datetime.datetime.now().strftime("%H:%M:%S %Z")
+	if ENABLE_TWEETING:
+		api.update_status("viewers: "+ data +" (change of):"+ (data-oldata) +" people, time: " +  time +" #redbullstratos") # string formatting!!
+	else:
+		open(OUTPUT + ".csv", "a").write(data +", " +  time + "\n")
+	print data + ", " + time # Kinda debugging info 
+	oldata = int(data)
+	time.sleep(REQUEST_INTERVALE)
+
